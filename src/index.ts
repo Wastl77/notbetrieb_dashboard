69c359ad515853145c369e70aaac16a60fb5184f
Sebastian Beier <wastl737@gmail.com>
2023-07-26 15:20:55 +0200

    Add html and css, script and route.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
+
+app.use(
+	express.static('/Users/sebastianbeier/Code/notbetrieb_dashboard/static'),
+);
+
+app.get('/', (req: Request, res: Response) => {
+	res.sendFile(
+		'/Users/sebastianbeier/Code/notbetrieb_dashboard/src/index.html',
+	);
+});
 
 app.listen(8000, () => {
 	console.log('listening on port 8000');
